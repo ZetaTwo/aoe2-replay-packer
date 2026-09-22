85d266275fdc56bf1564bf36c1ed77dea8f4ec9a
Calle Svensson <calle.svensson@zeta-two.com>
2026-09-23 00:20:46 +0100

    Only explicit chunk for unidecode

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,6 @@ export default defineConfig({
             if (id.includes('unidecode')) {
               return 'vendor_unidecode'
             }
-            return 'vendor'
           }
         }
       }
